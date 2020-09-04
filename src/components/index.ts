aa429c817242f1461d81855e4419c84a8c22492a
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-04 00:20:41 -0700

    prepare account form to be the guineau pig for the formik implementation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as App } from './App';
+export { default as Form } from './Form';
 export { default as MessageModal } from './MessageModal';
 export { default as ProtectedRoute } from './ProtectedRoute';
---
+++ src/pages/Account/AccountForm/AccountForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AccountFormProps } from './types';
+import { Form } from '../../../components';
+import './AccountForm.scss';
+
+class AccountForm extends React.PureComponent<AccountFormProps> {
+    render() {
+        return undefined;
+    }
+}
+
+export default AccountForm;
