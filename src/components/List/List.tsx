015c1d52d37176306ca908f62afea0084688dad3
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-08 22:48:26 -0700

    working on following bvaughn for implementing list using react-window

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FixedSizeList } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
+import InfiniteLoader from 'react-window-infinite-loader';
 import { ListProps, ListState, RowRendererProps } from './types';
 import defaultRowRenderer from './defaultRowRenderer';
 import './List.scss';
@@ -12,25 +13,52 @@ class List extends React.PureComponent<ListProps, ListState> {
         super(props);
         this.rowRenderer = props.rowRenderer || defaultRowRenderer;
     }
+
+    // loadMoreItems = () => {
+    //     const { isFetching, loadMore } = this.props;
+    //     if (!isFetching) {
+    //         loadMore();
+    //     }
+    // };
+
+    loadMoreItems = (startIndex: number, stopIndex: number): Promise<void> => {
+        const { loadMore } = this.props;
+        return new Promise((resolve) =>
+            loadMore(startIndex, stopIndex, resolve)
+        );
+    };
+
+    isItemLoaded = (index: number) => {
+        const { loadCount } = this.props;
+        return index <= loadCount;
+    };
+
     render() {
-        const { itemCount, itemSize, itemData } = this.props;
+        const { count, size, data } = this.props;
         console.log(this.props);
 
         return (
-            <AutoSizer>
-                {({ height, width }) => (
-                    <FixedSizeList
-                        className="List"
-                        height={height}
-                        itemCount={itemCount}
-                        itemSize={itemSize}
-                        width={width}
-                        itemData={itemData}
-                    >
-                        {this.rowRenderer}
-                    </FixedSizeList>
+            <InfiniteLoader
+                isItemLoaded={this.isItemLoaded}
+                loadMoreItems={this.loadMoreItems}
+                itemCount={count}
+            >
+                {({ onItemsRendered, ref }) => (
+                    <section>
+                        <FixedSizeList
+                            className="List"
+                            itemCount={count}
+                            itemSize={size}
+                            onItemsRendered={onItemsRendered}
+                            height={600}
+                            width={600}
+                            ref={ref}
+                        >
+                            {this.rowRenderer}
+                        </FixedSizeList>
+                    </section>
                 )}
-            </AutoSizer>
+            </InfiniteLoader>
         );
     }
 }
