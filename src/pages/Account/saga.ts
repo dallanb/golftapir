97aec02dcd6c2e298ed655aff7713fba1862ac94
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-09-18 11:21:05 -0700

    create helper generators for my main page sagas

--- src/pages/Account/saga.ts
+++ src/pages/Account/saga.ts
@@ -1,27 +1,22 @@
-import { all, put, race, select, take, takeLatest } from 'redux-saga/effects';
+import {
+    all,
+    call,
+    put,
+    race,
+    select,
+    take,
+    takeLatest,
+} from 'redux-saga/effects';
 import { AccountActions, AccountTypes, ContestTypes } from '@actions';
 import CONSTANTS from '@locale/en-CA';
 import AccountPageActions, { AccountPageTypes } from './actions';
 import { selectData } from '@selectors/AuthSelectors';
 import { prepareInitialValues } from '@pages/Account/utils';
 
+// Action Handler
 function* init() {
     try {
-        yield put(
-            AccountActions.fetchAccount('me', {
-                include: 'phone,address,avatar',
-            })
-        );
-        const { success, failure } = yield race({
-            success: take(AccountTypes.FETCH_ACCOUNT_SUCCESS),
-            failure: take(AccountTypes.FETCH_ACCOUNT_FAILURE),
-        });
-
-        if (failure) {
-            throw new Error(CONSTANTS.CONTEST.ERROR.FETCH);
-        }
-
-        const { data } = success;
+        const { data } = yield call(fetchMyAccount);
         const authData = yield select(selectData);
         const updateFormInitialValues = prepareInitialValues(data, authData);
         yield put(AccountPageActions.set({ data, updateFormInitialValues }));
@@ -30,6 +25,25 @@ function* init() {
     } catch (err) {
         yield put(AccountPageActions.initFailure(err));
     }
+}
+
+// Helper
+function* fetchMyAccount() {
+    yield put(
+        AccountActions.fetchAccount('me', {
+            include: 'phone,address,avatar',
+        })
+    );
+    const { success, failure } = yield race({
+        success: take(AccountTypes.FETCH_ACCOUNT_SUCCESS),
+        failure: take(AccountTypes.FETCH_ACCOUNT_FAILURE),
+    });
+
+    if (failure) {
+        throw new Error(CONSTANTS.CONTEST.ERROR.FETCH);
+    }
+
+    return success;
 }
 
 export default function* AccountPageSaga() {
--- src/pages/Contests/saga.ts
+++ src/pages/Contests/saga.ts
@@ -1,23 +1,36 @@
-import { all, put, race, select, take, takeLatest } from 'redux-saga/effects';
+import {
+    all,
+    call,
+    put,
+    race,
+    select,
+    take,
+    takeLatest,
+} from 'redux-saga/effects';
 import ContestsPageActions, { ContestsPageTypes } from './actions';
 import { ContestActions, ContestTypes } from '@actions';
 import CONSTANTS from '@locale/en-CA';
 
+// Action Handler
 function* init() {
     try {
-        yield put(ContestActions.fetchContests());
-        const { failure } = yield race({
-            success: take(ContestTypes.FETCH_CONTESTS_SUCCESS),
-            failure: take(ContestTypes.FETCH_CONTESTS_FAILURE),
-        });
-
-        if (failure) {
-            throw new Error(CONSTANTS.CONTEST.ERROR.FETCH);
-        }
-
+        yield call(fetchContests);
         yield put(ContestsPageActions.initSuccess());
     } catch (err) {
         yield put(ContestsPageActions.initFailure(err));
+    }
+}
+
+// Helpers
+function* fetchContests() {
+    yield put(ContestActions.fetchContests());
+    const { failure } = yield race({
+        success: take(ContestTypes.FETCH_CONTESTS_SUCCESS),
+        failure: take(ContestTypes.FETCH_CONTESTS_FAILURE),
+    });
+
+    if (failure) {
+        throw new Error(CONSTANTS.CONTEST.ERROR.FETCH);
     }
 }
 
