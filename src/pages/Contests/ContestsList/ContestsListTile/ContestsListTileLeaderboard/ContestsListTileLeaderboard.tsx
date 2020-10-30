463da9d6285fbb2100e94d163910090ce61aa514
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-10-29 21:50:58 -0700

    update leaderboard to handle completed status

--- src/pages/Contests/ContestsList/ContestsListTile/ContestsListTileLeaderboard/ContestsListTileLeaderboard.tsx
+++ src/pages/Contests/ContestsList/ContestsListTile/ContestsListTileLeaderboard/ContestsListTileLeaderboard.tsx
@@ -1,33 +1,23 @@
 import React from 'react';
-import { keyBy as _keyBy } from 'lodash';
 import { ContestsListTileLeaderboardProps } from './types';
-import './ContestsListTileLeaderboard.scss';
 import constants from '@constants';
+import { findLowestScoringParticipant } from '@pages/Contests/utils';
+import './ContestsListTileLeaderboard.scss';
 
 const ContestsListTileLeaderboard: React.FunctionComponent<ContestsListTileLeaderboardProps> = ({
     status,
     participants,
 }) => {
-    if (
-        status !== constants.STATUS.PENDING.KEY &&
-        status !== constants.STATUS.COMPLETED.KEY
-    ) {
-        const participantsHash = _keyBy(participants, 'score');
-        const participantKeys = Object.keys(participantsHash).reduce(
-            (accum: number[], key: string) => {
-                const val = parseInt(key);
-                if (!isNaN(val)) {
-                    accum.push(val);
-                }
-                return accum;
-            },
-            []
-        );
-        const lowestParticipant = Math.min(...participantKeys);
-        const participant = participantsHash[lowestParticipant];
+    if (status !== constants.STATUS.PENDING.KEY) {
+        const participant = findLowestScoringParticipant(participants);
         if (participant) {
-            const participantName = `${participant.first_name} ${participant.last_name}`;
-            return <div>Current Leader: {participantName}</div>;
+            if (status !== constants.STATUS.COMPLETED.KEY) {
+                const participantName = `${participant.first_name} ${participant.last_name}`;
+                return <div>Current Leader: {participantName}</div>;
+            } else {
+                const participantName = `${participant.first_name} ${participant.last_name}`;
+                return <div>Winner: {participantName}</div>;
+            }
         }
     }
     return null;
