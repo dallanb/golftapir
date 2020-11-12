6a37ca2834abe38f298521933b7cae664c2655f0
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-11-11 23:45:38 -0800

    update contest sider styling to move buttons to the bottom

--- src/pages/Contest/ContestSider/ContestSiderPending/ContestSiderPending.tsx
+++ src/pages/Contest/ContestSider/ContestSiderPending/ContestSiderPending.tsx
@@ -30,7 +30,6 @@ const ContestSiderPending: React.FunctionComponent<ContestSiderPendingProps> = (
 
     return (
         <div className="contest-sider-pending">
-            PLEASE RESPOND TO CONTEST REQUEST BELOW
             <div className="contest-sider-pending-buttons">
                 <div className="contest-sider-pending-buttons-button active">
                     <Button block type="primary" onClick={handleAcceptClick}>
