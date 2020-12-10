bc9f6f031aabe760a4a0628b39287c6d0f013806
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-12-10 14:51:58 -0800

    added a chevron for trend of a user's live score

---
+++ src/pages/Contest/ContestContent/ContestLeaderboard/ContestLeaderboardTable/ContestLeaderboardTablePosition/ContestLeaderboardTablePosition.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CaretDownFilled, CaretUpFilled } from '@ant-design/icons/lib';
+import { ContestLeaderboardTablePositionProps } from './types';
+import './ContestLeaderboardTablePosition.scss';
+
+const ContestLeaderboardTablePosition: React.FunctionComponent<ContestLeaderboardTablePositionProps> = ({
+    row: { original },
+}) => {
+    const { rank, count, trend } = original;
+    const prefix = count > 1 ? 'T' : '';
+    const pos = `${prefix}${rank}`;
+
+    const renderTrendIcon = (key: string) => {
+        switch (key) {
+            case 'upward':
+                return (
+                    <CaretUpFilled className="contest-leaderboard-table-position-icon-up" />
+                );
+            case 'downward':
+                return (
+                    <CaretDownFilled className="contest-leaderboard-table-position-icon-down" />
+                );
+            // case 'sideway':
+        }
+    };
+    return (
+        <div className="contest-leaderboard-table-position">
+            <div className="contest-leaderboard-table-position-icon">
+                {renderTrendIcon(trend)}
+            </div>
+            <div className="contest=leaderboard-table-position-position">
+                {pos}
+            </div>
+        </div>
+    );
+};
+export default ContestLeaderboardTablePosition;
