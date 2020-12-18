08de83f4096093a120f57c340cf959ca3c2919af
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-12-17 22:15:04 -0800

    clean up scorecard component in contest page

--- src/pages/Contest/ContestContent/ContestLeaderboard/ContestLeaderboardTable/ContestLeaderboardTableScorecard/ScorecardParticipant/ScorecardParticipant.tsx
+++ src/pages/Contest/ContestContent/ContestLeaderboard/ContestLeaderboardTable/ContestLeaderboardTableScorecard/ScorecardParticipant/ScorecardParticipant.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { get as _get } from 'lodash';
+import { Button } from 'antd';
 import { ScorecardParticipantProps } from './types';
 import { Avatar } from '@components';
 import { selectAccountsHash } from '@pages/Contest/selector';
 import { getName, withS3URL } from '@utils';
 import './ScorecardParticipant.less';
+import constants from '@constants';
 
 const ScorecardParticipant: React.FunctionComponent<ScorecardParticipantProps> = ({
     user,
 }) => {
-    console.log(user);
+    const history = useHistory();
     const accountsHash = useSelector(selectAccountsHash);
     const account = _get(accountsHash, [user], null);
     const avatar = _get(account, ['avatar', 's3_filename'], null);
     const name = getName(account, '');
     return (
         <div className="scorecard-participant">
-            <div className="scorecard-participant-avatar">
-                <Avatar src={avatar && withS3URL(avatar)} name={name} />
+            <div className="scorecard-participant-user">
+                <div className="scorecard-participant-user-avatar">
+                    <Avatar
+                        src={avatar && withS3URL(avatar)}
+                        name={name}
+                        size={84}
+                    />
+                </div>
+                <div className="scorecard-participant-user-name">{name}</div>
             </div>
-            <div className="scorecard-participant-name">{name}</div>
+            <div className="scorecard-participant-buttons">
+                <div className="scorecard-participant-button message">
+                    <Button block size={'small'} type="primary" key="message">
+                        Message
+                    </Button>
+                </div>
+                <div className="scorecard-participant-button profile">
+                    <Button
+                        block
+                        size={'small'}
+                        type="primary"
+                        key="profile"
+                        onClick={() =>
+                            history.push(
+                                `/app${constants.ROUTES.COMPETITOR.ROUTE}`,
+                                { ...account }
+                            )
+                        }
+                    >
+                        Profile
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
