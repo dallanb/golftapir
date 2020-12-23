4b82d6d54bea873b6de012d62df0cdf2ff22527a
Dallan Bhatti <dallan.bhatti@techtapir.com>
2020-12-23 10:01:02 -0800

    update stupid bug i was having with webpack build

--- src/components/Form/components/UploadField/UploadField.tsx
+++ src/components/Form/components/UploadField/UploadField.tsx
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { get as _get } from 'lodash';
 import { PlusOutlined } from '@ant-design/icons/lib';
 import { UploadFieldProps } from './types';
