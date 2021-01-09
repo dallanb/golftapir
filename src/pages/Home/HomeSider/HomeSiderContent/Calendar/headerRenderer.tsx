6cb9a3e7968eede2a1c256c788c71b62fadd46d5
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-08 17:32:11 -0800

    replace calender text with selectors

--- src/pages/Home/HomeSider/HomeSiderContent/Calendar/headerRenderer.tsx
+++ src/pages/Home/HomeSider/HomeSiderContent/Calendar/headerRenderer.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Col, Row, Select } from 'antd';
-import { CalenderHeaderProps } from './types';
 import { range as _range } from 'lodash';
+import {
+    DoubleLeftOutlined,
+    DoubleRightOutlined,
+    LeftOutlined,
+    RightOutlined,
+} from '@ant-design/icons/lib';
+import { Button, Select } from 'antd';
+import { CalenderHeaderProps } from './types';
 
 const headerRenderer: React.FunctionComponent<CalenderHeaderProps> = ({
     value,
@@ -13,7 +19,7 @@ const headerRenderer: React.FunctionComponent<CalenderHeaderProps> = ({
     const localeData = value.localeData();
     const monthOptions = _range(0, 12).map((i) => {
         current.month(i);
-        const value = localeData.monthsShort(current);
+        const value = localeData.months(current);
         return (
             <Select.Option className="month-item" key={i} value={i}>
                 {value}
@@ -23,43 +29,59 @@ const headerRenderer: React.FunctionComponent<CalenderHeaderProps> = ({
     const month = value.month();
 
     const year = value.year();
-    const options = _range(year - 10, year + 10).map((i) => (
+    const options = _range(year, year + 2).map((i) => (
         <Select.Option key={i} value={i} className="year-item">
             {i}
         </Select.Option>
     ));
     return (
-        <div style={{ padding: 8 }}>
-            <Row gutter={8}>
-                <Col>
-                    <Select
-                        size="small"
-                        dropdownMatchSelectWidth={false}
-                        className="my-year-select"
-                        onChange={(newYear) => {
-                            const now = value.clone().year(newYear);
-                            onChange(now);
-                        }}
-                        value={String(year)}
-                    >
-                        {options}
-                    </Select>
-                </Col>
-                <Col>
-                    <Select
-                        size="small"
-                        dropdownMatchSelectWidth={false}
-                        defaultValue={localeData.monthsShort(value)}
-                        onChange={(selectedMonth) => {
-                            const newValue = value.clone();
-                            newValue.month(parseInt(selectedMonth, 10));
-                            onChange(newValue);
-                        }}
-                    >
-                        {monthOptions}
-                    </Select>
-                </Col>
-            </Row>
+        <div className="calendar-header">
+            <div className="calendar-header-left-buttons">
+                <Button type="text" className="header-button">
+                    <DoubleLeftOutlined />
+                </Button>
+                <Button type="text" className="header-button">
+                    <LeftOutlined />
+                </Button>
+            </div>
+            <div className="calendar-header-selectors">
+                <Select
+                    size="small"
+                    dropdownMatchSelectWidth={false}
+                    defaultValue={localeData.months(value)}
+                    onChange={(selectedMonth) => {
+                        const newValue = value.clone();
+                        newValue.month(parseInt(selectedMonth, 10));
+                        onChange(newValue);
+                    }}
+                    bordered={false}
+                    showArrow={false}
+                >
+                    {monthOptions}
+                </Select>
+                <Select
+                    size="small"
+                    dropdownMatchSelectWidth={false}
+                    className="my-year-select"
+                    onChange={(newYear) => {
+                        const now = value.clone().year(newYear);
+                        onChange(now);
+                    }}
+                    value={String(year)}
+                    bordered={false}
+                    showArrow={false}
+                >
+                    {options}
+                </Select>
+            </div>
+            <div className="calendar-header-right-buttons">
+                <Button type="text" className="header-button">
+                    <RightOutlined />
+                </Button>
+                <Button type="text" className="header-button">
+                    <DoubleRightOutlined />
+                </Button>
+            </div>
         </div>
     );
 };
