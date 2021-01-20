bd70e59c075deb293b22830d8f2fef232c4242eb
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-20 14:47:25 -0800

    fix reload bug with logout and login

--- src/pages/Home/HomeContent/HomeContent.tsx
+++ src/pages/Home/HomeContent/HomeContent.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { ContentLayoutContent } from '@layouts';
 import { HomeContentProps } from './types';
 import Leagues from './Leagues';
 import MemberInfo from './MemberInfo';
+import { selectIsInitialized } from '@apps/MemberApp/selector';
 
 const HomeContent: React.FunctionComponent<HomeContentProps> = ({}) => {
+    const isInitialized = useSelector(selectIsInitialized);
     return (
-        <ContentLayoutContent>
+        <ContentLayoutContent showSpinner={!isInitialized}>
             <>
                 <MemberInfo />
                 <Leagues />
--- src/pages/Home/HomeContent/Leagues/Leagues.tsx
+++ src/pages/Home/HomeContent/Leagues/Leagues.tsx
@@ -1,5 +1,8 @@
 import React, { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { isNil as _isNil } from 'lodash';
 import ComponentContent from '@layouts/ComponentContent';
+import { selectLeagues } from '@selectors/BaseSelector';
 import LeaguesList from './LeaguesList';
 import { LeaguesProps } from './types';
 import CONSTANTS from '@locale/en-CA';
@@ -7,9 +10,11 @@ import './Leagues.less';
 
 const Leagues: React.FunctionComponent<LeaguesProps> = () => {
     const ref = useRef(null);
+    const leagues = useSelector(selectLeagues);
     return (
         <ComponentContent
             title={CONSTANTS.PAGES.HOME.LEAGUES}
+            showSpinner={_isNil(leagues)}
             className="leagues"
         >
             <LeaguesList containerRef={ref} />
