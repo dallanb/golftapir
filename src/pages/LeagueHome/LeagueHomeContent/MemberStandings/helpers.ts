9b96c0fbda678807d4ce8c86876de708b011f653
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-01-26 14:43:01 -0800

    working on updating to new styling format

--- src/pages/LeagueHome/LeagueHomeContent/MemberStandings/helpers.ts
+++ src/pages/LeagueHome/LeagueHomeContent/MemberStandings/helpers.ts
@@ -4,7 +4,11 @@ import { selectLeagueUUID } from '@apps/LeagueApp/selector';
 import LeagueHomePageContentMemberStandingsActions from './actions';
 
 export function* initMemberStandings(
-    options: any = { include: 'stat,avatar', sort_by: 'win_count.desc' }
+    options: any = {
+        include: 'stat,avatar',
+        status: 'active',
+        sort_by: 'win_count.desc',
+    }
 ) {
     const uuid = yield select(selectLeagueUUID);
     const { members } = yield call(MemberService.fetchMemberStandings, {
