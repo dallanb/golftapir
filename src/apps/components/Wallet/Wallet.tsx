758c237e58df2592fd193bb81d1871ec1fb8753e
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-03 10:08:45 -0800

    start updating wallet and user tile to be content in the sider and also be component content

--- src/apps/components/Wallet/Wallet.tsx
+++ src/apps/components/Wallet/Wallet.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Button } from 'antd';
+import { DollarTwoTone, PlusOutlined } from '@ant-design/icons';
 import { WalletProps } from './types';
-import SiderLayoutHeader from '@layouts/SiderLayout/SiderLayoutHeader';
-import HeaderTitle from './HeaderTitle';
-import HeaderExtra from './HeaderExtra';
-import { selectMeIsInitialized } from '@selectors/BaseSelector';
+import ComponentContent from '@layouts/ComponentContent';
+import {
+    selectMeIsInitialized,
+    selectMyWalletBalance,
+} from '@selectors/BaseSelector';
 import './Wallet.less';
 
 const Wallet: React.FunctionComponent<WalletProps> = () => {
     const isInitialized = useSelector(selectMeIsInitialized);
+    const balance = useSelector(selectMyWalletBalance);
     return (
-        <SiderLayoutHeader
-            title={<HeaderTitle />}
-            extra={<HeaderExtra />}
-            className="wallet-sider-layout-header"
-            showSpinner={!isInitialized}
-        />
+        <ComponentContent className="wallet" showSpinner={!isInitialized}>
+            <div className="wallet-main">
+                <div className="wallet-balance">
+                    <div className="wallet-balance-label">Your Balance</div>
+                    <div className="wallet-balance-amount">
+                        <DollarTwoTone
+                            twoToneColor={'orange'}
+                            className="wallet-balance-amount-icon"
+                        />
+                        {balance}
+                    </div>
+                </div>
+            </div>
+            <div className="wallet-side">
+                <div className="wallet-side-button">
+                    <Button
+                        onClick={() => null}
+                        type="default"
+                        shape="round"
+                        icon={<PlusOutlined />}
+                    >
+                        Add
+                    </Button>
+                </div>
+            </div>
+        </ComponentContent>
     );
 };
 
