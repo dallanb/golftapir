0c7cf6b2081fbda84816d098dddf4c3a71ccef65
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-07 18:59:54 -0800

    remove spectator text

--- src/pages/Contest/ContestSider/ContestSiderContent/ParticipantSpectator/ParticipantSpectator.tsx
+++ src/pages/Contest/ContestSider/ContestSiderContent/ParticipantSpectator/ParticipantSpectator.tsx
@@ -3,11 +3,7 @@ import { ParticipantSpectatorProps } from './types';
 import './ParticipantSpectator';
 
 const ParticipantSpectator: React.FunctionComponent<ParticipantSpectatorProps> = () => {
-    return (
-        <div className="participant-spectator">
-            SIT BACK AND ENJOY BIG FELLA
-        </div>
-    );
+    return null;
 };
 
 export default ParticipantSpectator;
