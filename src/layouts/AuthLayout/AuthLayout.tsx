8e513df619d5910515212e5868b6bae98ba8a3fb
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-26 10:44:29 -0700

    fix issues with auth layout for all devices

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -10,7 +10,9 @@ const AuthLayout: React.FunctionComponent<AuthLayoutProps> = ({ children }) => {
     return (
         <Layout className="auth-layout-view glassmorphic">
             <Content className="auth-layout-view-content">
-                <div className="auth-layout-title" />
+                <div className="auth-layout-title-wrapper">
+                    <div className="auth-layout-title" />
+                </div>
                 <div className="auth-layout-content">{children}</div>
             </Content>
         </Layout>
