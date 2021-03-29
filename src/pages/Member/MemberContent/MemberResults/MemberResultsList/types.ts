e65386620b0cedda6ea0ae7eaea364a56229e5be
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-29 10:29:06 -0700

    fix height issue with contests page

--- src/pages/Member/MemberContent/MemberResults/MemberResultsList/types.ts
+++ src/pages/Member/MemberContent/MemberResults/MemberResultsList/types.ts
@@ -2,6 +2,7 @@ import React from 'react';
 
 export interface MemberResultsListProps {
     containerRef: React.Ref<any>;
+    containerDimensions: any;
     data: any;
     metadata: any;
     options: any;
