82693cf986f0ebd040a30a8d5f79e12bfd5f6d5b
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-04-03 14:37:08 -0700

    going to try a new approach for handling resize of the page

--- src/pages/Notifications/NotificationsContent/Notifications/Notifications.tsx
+++ src/pages/Notifications/NotificationsContent/Notifications/Notifications.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { get as _get } from 'lodash';
 import NotificationsList from './NotificationsList';
 import { NotificationsProps } from './types';
 import NotificationsPageContentNotificationsActions from './actions';
 import { selectData } from './selector';
 import ComponentContent from '@layouts/ComponentContent';
-import './Notifications.less';
 import CONSTANTS from '@locale/en-CA';
+import { ResizeContext } from '@contexts';
+import './Notifications.less';
 
 const Notifications: React.FunctionComponent<NotificationsProps> = ({}) => {
     const dispatch = useDispatch();
     const ref = useRef(null);
+    const dimensions = useContext(ResizeContext);
+    console.log(_get(dimensions, ['height']));
 
     useEffect(() => {
         dispatch(NotificationsPageContentNotificationsActions.init());
